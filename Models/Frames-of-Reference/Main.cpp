685fe8852471e5545aa4af69108cb43558742de8
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-13 16:22:53 -0500

    added orientation to grammar

--- Models/Frames-of-Reference/Main.cpp
+++ Models/Frames-of-Reference/Main.cpp
@@ -51,7 +51,7 @@ class MyGrammar : public Grammar<Scene,bool,   Scene,bool,Object,Vector, double>
 public:
 	MyGrammar() {
 		add("location(%s)",       +[](Object x) -> Vector { return x.location; });
-		
+		add("orientation(%s)", +[](Object x) -> Vector {return x.orientation;});
 		add("distance(%s,%s)",    +[](Vector x, Vector y) -> double { 
 			// TODO: Compute distance..
 		});
