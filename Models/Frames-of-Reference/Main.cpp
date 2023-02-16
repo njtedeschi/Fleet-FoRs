d16415613f5a94be17eef3950c0044d8200b2b90
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 13:06:28 -0500

    added target concept for behind

--- Models/Frames-of-Reference/Main.cpp
+++ Models/Frames-of-Reference/Main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char** argv){
         target["above"] = InnerHypothesis(grammar.simple_parse("parallel(displacement(ground(x),figure(x)),up(x))"));
         target["below"] = InnerHypothesis(grammar.simple_parse("parallel(displacement(figure(x),ground(x)),up(x))"));
         target["front"] = InnerHypothesis(grammar.simple_parse("or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),parallel(displacement(figure(x),ground(x)),orientation(speaker(x))))"));
-        target["behind"] = InnerHypothesis(grammar.simple_parse(""));
+        target["behind"] = InnerHypothesis(grammar.simple_parse("or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),parallel(displacement(ground(x),figure(x)),orientation(speaker(x))))"));
         target["side"] = InnerHypothesis(grammar.simple_parse(""));
 	
 	//------------------
