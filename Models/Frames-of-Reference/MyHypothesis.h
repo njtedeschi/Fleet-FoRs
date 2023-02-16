78fbe9d942299efcf58e234ec5c0111b44853a98
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 09:51:18 -0500

    outline of methods for MyHypothesis

--- Models/Frames-of-Reference/MyHypothesis.h
+++ Models/Frames-of-Reference/MyHypothesis.h
@@ -6,11 +6,26 @@
 class InnerHypothesis : public DeterministicLOTHypothesis<InnerHypothesis,MyInput,bool,MyGrammar,&grammar>,
                         public CachedCallHypothesis<InnerHypothesis,MyInput,bool>  {
 
-}
+};
 
 #include "Lexicon.h"
 
 struct ignore_t  {}; /// we don't need inputs/outputs for out MyHypothesis
 class MyHypothesis : public Lexicon<MyHypothesis, std::string, InnerHypothesis, ignore_t,ignore_t, MyInput> {
 
-}
+    using Super = Lexicon<MyHypothesis, std::string, InnerHypothesis, ignore_t, ignore_t, MyInput>;
+    using Super::Super; // inherit the constructors
+
+public:
+
+    void clear_cache(){
+        // Is this necessary?
+    }
+
+    virtual double compute_likelihood(const data_t& data, double breakout=-infinity) override {
+
+    }
+
+    // Printing functions: string and show
+
+};
