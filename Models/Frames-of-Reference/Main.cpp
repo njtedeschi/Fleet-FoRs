4fc67ee3b93bb423909c21c5d033e708f72a4d5e
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 11:59:09 -0500

    big update: refactoring data sampling

--- Models/Frames-of-Reference/Main.cpp
+++ Models/Frames-of-Reference/Main.cpp
@@ -50,8 +50,26 @@ MyHypothesis target;
 #include "Random.h"
 #include "Builtins.h"
 
-MyHypothesis::datum_t sample_datum() {
+std::mt19937 engine(0); // RNG with set seed
+double p_direct = 0.2; // probability a scene is direct
+std::bernoulli_distribution direct_dist(p_direct);
+std::bernoulli_distribution wrong_dist(1-alpha_t);
 
+// Uniform dists over scenes (direct_scenes and nondirect_scenes defined in "Scene.h")
+std::uniform_int_distribution<int> direct_scene_dist(0, direct_scenes.size() - 1);
+std::uniform_int_distribution<int> nondirect_scene_dist(0, nondirect_scenes.size() - 1);
+
+MyHypothesis::datum_t sample_datum() {
+    // Sample scene
+    Scene scene;
+    if(direct_dist(engine)) {
+
+    } else {
+
+    }
+    std::string word;
+    // Sample word
+    return MyInput{.scene=scene, .word=word}
 }
 
 int main(int argc, char** argv){ 
@@ -75,81 +93,12 @@ int main(int argc, char** argv){
 
         // Sample
         int num_samples = 100;
-        std::mt19937 engine(0); // RNG with set seed
-        double p_direct = 0.2; // probability a scene is direct
-        std::bernoulli_distribution direct_dist(p_direct);
-        double p_wrong = 0.05; // probability a scene is labeled incorrectly
-        std::bernoulli_distribution wrong_dist(p_wrong);
-
-        // Uniform dists over scenes (direct_scenes and nondirect_scenes defined in "Scene.h")
-        std::uniform_int_distribution<int> direct_scene_dist(0, direct_scenes.size() - 1);
-        std::uniform_int_distribution<int> nondirect_scene_dist(0, nondirect_scenes.size() - 1);
 
         for (int i = 0; i < num_samples; i++){
-            // Sample scene
-            Scene scene; bool label;
-            if (direct_dist(engine)) {
-                scene = direct_scenes[direct_scene_dist(engine)];
-                label = behind_direct(scene);
-            } else {
-                scene = nondirect_scenes[nondirect_scene_dist(engine)];
-                label = behind_nondirect(scene);
-            }
-            // Add noise
-            if (wrong_dist(engine)) {
-                    label = !label;
-                    }
-            mydata.emplace_back(scene, label, 1-p_wrong);
+            MyInput input = sample_datum();
+            mydata.emplace_back(input, true, 1-alpha_t);
         }
 
-
-        /* Scene direct_behind = {direct_speaker, direct_speaker, west_figure}; */
-        /* Scene direct_front = {direct_speaker, direct_speaker, east_figure}; */
-        /* Scene direct_left = {direct_speaker, direct_speaker, north_figure}; */
-        /* Scene direct_right = {direct_speaker, direct_speaker, south_figure}; */
-        /* Scene direct_above = {direct_speaker, direct_speaker, up_figure}; */
-        /* Scene direct_below = {direct_speaker, direct_speaker, down_figure}; */
-
-        /* Scene front_0_int = {nondirect_speaker, east_facing_ground, east_figure}; */
-        /* Scene behind_0_int = {nondirect_speaker, east_facing_ground, west_figure}; */
-        /* Scene front_90_int = {nondirect_speaker, north_facing_ground, north_figure}; */
-        /* Scene front_90_rel ={nondirect_speaker, north_facing_ground, west_figure}; */
-        /* Scene behind_90_int = {nondirect_speaker, north_facing_ground, south_figure}; */
-        /* Scene behind_90_rel = {nondirect_speaker, north_facing_ground, east_figure}; */
-        /* Scene front_180_int = {nondirect_speaker, west_facing_ground, west_figure}; */
-        /* Scene behind_180_int = {nondirect_speaker, west_facing_ground, east_figure}; */
-
-        /* // Learning 'behind' */
-        /* mydata.emplace_back(direct_behind, true, 0.95); */
-        /* mydata.emplace_back(direct_front, false, 0.95); */
-        /* mydata.emplace_back(direct_left, false, 0.95); */
-        /* mydata.emplace_back(direct_above, false, 0.95); */
-        /* mydata.emplace_back(direct_below, false, 0.95); */
-        /* mydata.emplace_back(front_0_int, true, 0.95); */
-        /* mydata.emplace_back(behind_0_int, true, 0.95); */
-        /* mydata.emplace_back(front_90_int, false, 0.95); */
-        /* mydata.emplace_back(front_90_rel, false, 0.95); */
-        /* mydata.emplace_back(behind_90_int, true, 0.95); */
-        /* mydata.emplace_back(behind_90_rel, true, 0.95); */
-        /* mydata.emplace_back(front_180_int, false, 0.95); */
-        /* mydata.emplace_back(behind_180_int, true, 0.95); */
-
-
-	/* for(int i=0;i<10;i++) { */
-		
-	/* 	if(flip(0.8)) { */
-	/* 		mydata.emplace_back(....) */
-	/* 	} */
-	/* 	else { */
-			
-			
-	/* 	} */
-		
-	/* } */
-	//------------------
-	// Run
-	//------------------
-	
 	TopN<MyHypothesis> top;
 
 	auto h0 = MyHypothesis::sample();
