e43a51e87ed6b5f0321bc3fddfc88b3878a34524
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 10:44:49 -0500

    removed code that was copied over to Scene.h

--- Models/Frames-of-Reference/Main.cpp
+++ Models/Frames-of-Reference/Main.cpp
@@ -7,59 +7,7 @@ const std::vector<std::string> words = {"above", "below", "front", "behind", "si
 
 static const double alpha_t = 0.95; // probability of true description
 
-using Vector = std::array<double,3>;
-
-Vector operator*(double s, const Vector& v) {
-    Vector result;
-    for (int i = 0; i < 3; ++i) {
-        result[i] = v[i] * s;
-    }
-    return result;
-}
-
-Vector operator-(const Vector& v1, const Vector& v2) {
-    Vector result;
-    for (int i = 0; i < 3; ++i) {
-        result[i] = v1[i] - v2[i];
-    }
-    return result;
-}
-
-double dot_product(const Vector &a, const Vector &b){
-    double result = 0.0;
-    for(int i = 0; i < a.size(); i++){
-        result += a[i] * b[i];
-    }
-    return result;
-}
-
-double magnitude(const Vector &a){
-    double result = 0.0;
-    for (int i = 0; i < a.size(); i++){
-        result += a[i] * a[i];
-    }
-    return sqrt(result);
-}
-
-double cosine_similarity(const Vector &a, const Vector &b){
-    return dot_product(a, b)/(magnitude(a) * magnitude(b));
-}
-
-double angle_between(){
-    // TODO: implement
-}
-
-struct Object {
-	Vector location;
-	Vector orientation;
-	/* std::string name; */ 
-};
-
-struct Scene {
-	Object speaker;
-	Object ground;
-	Object figure; 
-};
+#include "Scene.h"
 
 struct MyInput {
     Scene scene;
