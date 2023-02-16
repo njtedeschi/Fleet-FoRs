d22d94f92024cb9d4311ee42b9a084f46707378e
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 10:00:01 -0500

    set up for InnerHypothesis

--- Models/Frames-of-Reference/MyHypothesis.h
+++ Models/Frames-of-Reference/MyHypothesis.h
@@ -5,6 +5,33 @@
 
 class InnerHypothesis : public DeterministicLOTHypothesis<InnerHypothesis,MyInput,bool,MyGrammar,&grammar>,
                         public CachedCallHypothesis<InnerHypothesis,MyInput,bool>  {
+public:
+	using Super = DeterministicLOTHypothesis<InnerHypothesis,MyInput,bool,MyGrammar,&grammar>;
+	using Super::Super; // inherit the constructors
+	using CCH = CachedCallHypothesis<InnerHypothesis,MyInput,bool>;
+	
+	InnerHypothesis(const InnerHypothesis& c) : Super(c), CCH(c) {}	
+	InnerHypothesis(const InnerHypothesis&& c) :  Super(c), CCH(c) { }	
+	
+	InnerHypothesis& operator=(const InnerHypothesis& c) {
+		Super::operator=(c);
+		CachedCallHypothesis::operator=(c);
+		return *this;
+	}
+	InnerHypothesis& operator=(const InnerHypothesis&& c) {
+		Super::operator=(c);
+		CachedCallHypothesis::operator=(c);
+		return *this;
+	}
+	
+	void set_value(Node&  v) { 
+		Super::set_value(v);
+		CachedCallHypothesis::clear_cache();
+	}
+	void set_value(Node&& v) { 
+		Super::set_value(v);
+		CachedCallHypothesis::clear_cache();
+	}
 
 };
 
