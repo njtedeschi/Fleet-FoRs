ec2b07f5a4b70f94701dc1810930cc93d0e26f3e
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-16 09:38:02 -0500

    copied grammar over to MyGrammar.h

---
+++ Models/Frames-of-Reference/MyGrammar.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Grammar.h"
+#include "Singleton.h"
+
+class MyGrammar : public Grammar<MyInput,bool,   MyInput,bool,Object,Vector, double>,
+				  public Singleton<MyGrammar> {
+public:
+	MyGrammar() {
+                add("displacement(%s,%s)", +[](Object x, Object y) -> Vector {
+                        return y.location - x.location;
+                        });
+		add("orientation(%s)", +[](Object x) -> Vector {return x.orientation;});
+                add("parallel(%s,%s)", +[](Vector x, Vector y) -> bool {
+                        if (magnitude(x) == 0 || magnitude(y) == 0) {return false;}
+                        return cosine_similarity(x,y) == 1;
+                        });
+                /* add("antiparallel(%s,%s)", +[](Vector x, Vector y) -> bool { */
+                /*         if (magnitude(x) == 0 || magnitude(y) == 0) {return false;} */
+                /*         return cosine_similarity(x,y) == -1; */
+                /*         }); */
+                add("orthogonal(%s,%s)", +[](Vector x, Vector y) -> bool {
+                        if (magnitude(x) == 0 || magnitude(y) == 0) {return false;}
+                        return cosine_similarity(x,y) == 0;
+                        });
+		
+                add("and(%s,%s)",    Builtins::And<MyGrammar>);
+		add("or(%s,%s)",     Builtins::Or<MyGrammar>);
+		add("not(%s)",       Builtins::Not<MyGrammar>);
+
+		add("speaker(%s)",       +[](MyInput x) -> Object { return x.scene.speaker; });
+		add("figure(%s)",        +[](MyInput x) -> Object { return x.scene.figure; });
+		add("ground(%s)",        +[](MyInput x) -> Object { return x.scene.ground; });
+		add("x",             Builtins::X<MyGrammar>);
+	}
+} grammar;
