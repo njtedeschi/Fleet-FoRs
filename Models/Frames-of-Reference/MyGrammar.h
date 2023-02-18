bdd2b9cc9ecccc7bd8f1486356cd68a6fcddfe77
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-18 14:33:28 -0500

    upweighted rules that produce vectors

--- Models/Frames-of-Reference/MyGrammar.h
+++ Models/Frames-of-Reference/MyGrammar.h
@@ -3,14 +3,16 @@
 #include "Grammar.h"
 #include "Singleton.h"
 
+double VECTOR_WEIGHT = 3.0;
+
 class MyGrammar : public Grammar<MyInput,bool,   MyInput,bool,Object,Vector, double>,
 				  public Singleton<MyGrammar> {
 public:
 	MyGrammar() {
                 add("displacement(%s,%s)", +[](Object x, Object y) -> Vector {
                         return y.location - x.location;
-                        });
-		add("orientation(%s)", +[](Object x) -> Vector {return x.orientation;});
+                        }, VECTOR_WEIGHT);
+		add("orientation(%s)", +[](Object x) -> Vector {return x.orientation;}, VECTOR_WEIGHT);
                 add("parallel(%s,%s)", +[](Vector x, Vector y) -> bool {
                         if (magnitude(x) == 0 || magnitude(y) == 0) {return false;}
                         return cosine_similarity(x,y) == 1;
@@ -28,7 +30,7 @@ public:
 		add("or(%s,%s)",     Builtins::Or<MyGrammar>);
 		add("not(%s)",       Builtins::Not<MyGrammar>);
 
-		add("up(%s)",       +[](MyInput x) -> Vector { Vector up = {0,0,1}; return up;});
+		add("up(%s)",       +[](MyInput x) -> Vector { Vector up = {0,0,1}; return up;}, VECTOR_WEIGHT);
 
 		add("speaker(%s)",       +[](MyInput x) -> Object { return x.scene.speaker; });
 		add("figure(%s)",        +[](MyInput x) -> Object { return x.scene.figure; });
