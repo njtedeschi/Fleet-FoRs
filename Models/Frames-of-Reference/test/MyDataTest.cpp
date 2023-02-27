c74d330a3e870db88ece994134aa6098de6e9561
Nico Tedeschi <nicotedeschi@gmail.com>
2023-02-27 14:48:45 -0500

    added set up for MyData object in MyDataTest

--- Models/Frames-of-Reference/test/MyDataTest.cpp
+++ Models/Frames-of-Reference/test/MyDataTest.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <unordered_map>
+
 #include "MyGrammar.h"
 #include "MyHypothesis.h"
 #include "MyData.h"
@@ -7,10 +9,16 @@
 class MyDataTest : public ::testing::Test {
     protected:
         void SetUp() override {
-            // formulas = ...
+            std::unordered_map<std::string, std::string> formulas = {
+            {"above", "parallel(displacement(ground(x),figure(x)),up(x))"},
+            {"below", "parallel(displacement(figure(x),ground(x)),up(x))"},
+            {"front", "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),parallel(displacement(figure(x),ground(x)),orientation(speaker(x))))"},
+            {"behind", "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),parallel(displacement(ground(x),figure(x)),orientation(speaker(x))))"},
+            {"side", "orthogonal(displacement(ground(x),figure(x)),orientation(ground(x)))"}
+        };
+            mydata = MyData(formulas);
             // other set up
         }
-        // mydata = MyData(formulas);
 };
 
 TEST_F(MyDataTest, ComputeTrueWords) {
