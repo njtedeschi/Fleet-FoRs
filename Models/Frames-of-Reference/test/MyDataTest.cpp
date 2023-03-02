29e437b6c7b12e51c11b95db81b991852e6e4ae9
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-02 14:47:57 -0500

    switched to using Catch2 for unit testing

--- Models/Frames-of-Reference/test/MyDataTest.cpp
+++ Models/Frames-of-Reference/test/MyDataTest.cpp
@@ -1,14 +1,31 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
+#include <set>
 #include <unordered_map>
 
-#include "MyGrammar.h"
-#include "MyHypothesis.h"
-#include "MyData.h"
+static const double alpha_t = 0.95;
 
-class MyDataTest : public ::testing::Test {
-    protected:
-        void SetUp() override {
+#include "../Scene.h"
+
+struct MyInput {
+    Scene scene;
+    std::string word;
+};
+
+#include "../MyGrammar.h"
+#include "../MyHypothesis.h"
+#include "../MyData.h"
+
+#include "TopN.h"
+#include "ParallelTempering.h"
+#include "Fleet.h" 
+#include "Random.h"
+#include "Builtins.h"
+
+class MyDataFixture {
+    public:
+        MyDataFixture() {
             std::unordered_map<std::string, std::string> formulas = {
             {"above", "parallel(displacement(ground(x),figure(x)),up(x))"},
             {"below", "parallel(displacement(figure(x),ground(x)),up(x))"},
@@ -17,10 +34,24 @@ class MyDataTest : public ::testing::Test {
             {"side", "orthogonal(displacement(ground(x),figure(x)),orientation(ground(x)))"}
         };
             mydata = MyData(formulas);
-            // other set up
         }
+        MyData mydata;
 };
 
-TEST_F(MyDataTest, ComputeTrueWords) {
-    // test code for compute_true_words
+/* TEST_CASE_METHOD(MyDataFixture, "MyData::words initializes from formulas map correctly" ) { */
+/*     std::vector<std::string> words = {"above", "below", "front", "behind", "side"}; */
+/*     REQUIRE(mydata.words == words); */
+/* } */
+
+TEST_CASE_METHOD(MyDataFixture, "MyData::compute_true_words returns expected words" ) { // test code for compute_true_words
+    /* Scene scene = {{{-2,0,0},{1,0,0}},{{0,0,0},{}},{{},{0,0,0}}}; */
+    Scene scene = {{{-2,0,0},{1,0,0}},{{0,0,0},{0,1,0}},{{1,0,0},{0,0,0}}};
+    std::set<std::string> computed_words = mydata.compute_true_words(scene);
+    std::set<std::string> expected_words = {"behind", "side"};
+    REQUIRE(expected_words == computed_words);
 }
+
+/* int main( int argc, char* argv[] ) { */
+/*     // Use Catch's default reporter */
+/*     Catch::Session().run(); */
+/* } */
