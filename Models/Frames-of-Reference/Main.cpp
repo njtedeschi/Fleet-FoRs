a564de4ddfb828d9204e4b6b6cc80a23ae637d4e
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-07 16:47:54 -0500

    semicolons

--- Models/Frames-of-Reference/Main.cpp
+++ Models/Frames-of-Reference/Main.cpp
@@ -62,11 +62,11 @@ int main(int argc, char** argv){
         // Front
         std::string front_int = "parallel(displacement(ground(x),figure(x)),orientation(ground(x)))";
         std::string front_int_rel = "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),parallel(displacement(figure(x),ground(x)),orientation(speaker(x))))";
-            std::string front_int_rel_not_behind = "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),and(parallel(displacement(figure(x),ground(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))" // No relative front when intrinsic behind
+            std::string front_int_rel_not_behind = "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),and(parallel(displacement(figure(x),ground(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))"; // No relative front when intrinsic behind
         // Behind
         std::string behind_int = "parallel(displacement(figure(x),ground(x)),orientation(ground(x)))";
         std::string behind_int_rel = "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),parallel(displacement(ground(x),figure(x)),orientation(speaker(x))))";
-            std::string behind_int_rel_not_front = "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),and(parallel(displacement(ground(x),figure(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))" // No relative behind when intrinsic front
+            std::string behind_int_rel_not_front = "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),and(parallel(displacement(ground(x),figure(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))"; // No relative behind when intrinsic front
         // Side
         std::string side_int = "orthogonal(displacement(ground(x),figure(x)),orientation(ground(x)))";
         std::string side_int_rel = "or(orthogonal(displacement(ground(x),figure(x)),orientation(ground(x))),orthogonal(displacement(ground(x),figure(x)),orientation(speaker(x))))";
