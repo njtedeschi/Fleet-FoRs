35b2d2fd8c22390d77c95b1711094ceb3f553ce1
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-08 14:38:38 -0500

    readded Vector based concept definitions as comments

--- Models/Frames-of-Reference/DSL.h
+++ Models/Frames-of-Reference/DSL.h
@@ -6,9 +6,24 @@ namespace DSL {
     const auto nonzero = +[](Vector x, Vector y) -> bool {
         return magnitude(x) == 0 || magnitude(y) == 0;
     };
+    /* const auto displacement = +[](Object x, Object y) -> Vector { */
+    /*     return y.position - x.position; */
+    /* }; */
     const auto displacement = +[](Object x, Object y) -> Displacement {
         return y.position - x.position;
     };
+    /* const auto parallel = +[](Vector x, Vector y) -> bool { */
+    /*     if (nonzero(x,y)) {return false;} */
+    /*     return cosine_similarity(x,y) == 1; */
+    /* }; */
+    /* const auto antiparallel = +[](Vector x, Vector y) -> bool { */
+    /*     if (nonzero(x,y)) {return false;} */
+    /*     return cosine_similarity(x,y) == -1; */
+    /* }; */
+    /* const auto orthogonal = +[](Vector x, Vector y) -> bool { */
+    /*     if (nonzero(x,y)) {return false;} */
+    /*     return cosine_similarity(x,y) == 0; */
+    /* }; */
     const auto parallel = +[](Displacement x, Direction y) -> bool {
         if (nonzero(x,y)) {return false;}
         return cosine_similarity(x,y) == 1;
