7e10c217f9054e7bffac8461f9d12230095f5ad0
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-08 14:36:24 -0500

    added separate weights for upward and righward orientations in grammar

--- Models/Frames-of-Reference/MyGrammar.h
+++ Models/Frames-of-Reference/MyGrammar.h
@@ -6,6 +6,8 @@
 #include "DSL.h"
 
 double VECTOR_WEIGHT = 3.0;
+double UPWARD_WEIGHT = 0.75;
+double RIGHTWARD_WEIGHT = 0.5;
 
 class MyGrammar : public Grammar<MyInput,bool,   MyInput,bool,Object,Vector, Position, Direction, Displacement, double>,
 				  public Singleton<MyGrammar> {
@@ -14,8 +16,8 @@ public:
                 add("displacement(%s,%s)", DSL::displacement, VECTOR_WEIGHT);
 
 		add("forward(%s)", DSL::forward, VECTOR_WEIGHT);
-		add("upward(%s)", DSL::upward, VECTOR_WEIGHT);
-		add("rightward(%s)", DSL::rightward, VECTOR_WEIGHT);
+		add("upward(%s)", DSL::upward, UPWARD_WEIGHT*VECTOR_WEIGHT);
+		add("rightward(%s)", DSL::rightward, RIGHTWARD_WEIGHT*VECTOR_WEIGHT);
 
                 add("parallel(%s,%s)", DSL::parallel);
                 /* add("antiparallel(%s,%s)", DSL::antiparallel); */
