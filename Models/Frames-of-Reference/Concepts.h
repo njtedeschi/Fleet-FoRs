109072e95e462a5432cadf61ac83eb0a81b971e3
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-08 09:53:40 -0500

    replaced orientation with forward in MyGrammar

--- Models/Frames-of-Reference/Concepts.h
+++ Models/Frames-of-Reference/Concepts.h
@@ -8,16 +8,16 @@ namespace Concepts {
     std::string below_abs = "parallel(displacement(figure(x),ground(x)),up(x))";
 
     // Front
-    std::string front_int = "parallel(displacement(ground(x),figure(x)),orientation(ground(x)))";
-    std::string front_int_rel = "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),parallel(displacement(figure(x),ground(x)),orientation(speaker(x))))";
-        std::string front_int_rel_not_behind = "or(parallel(displacement(ground(x),figure(x)),orientation(ground(x))),and(parallel(displacement(figure(x),ground(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))"; // No relative front when intrinsic behind
+    std::string front_int = "parallel(displacement(ground(x),figure(x)),forward(ground(x)))";
+    std::string front_int_rel = "or(parallel(displacement(ground(x),figure(x)),forward(ground(x))),parallel(displacement(figure(x),ground(x)),forward(speaker(x))))";
+        std::string front_int_rel_not_behind = "or(parallel(displacement(ground(x),figure(x)),forward(ground(x))),and(parallel(displacement(figure(x),ground(x)),forward(speaker(x))),not(parallel(forward(ground(x)),forward(speaker(x))))))"; // No relative front when intrinsic behind
 
     // Behind
-    std::string behind_int = "parallel(displacement(figure(x),ground(x)),orientation(ground(x)))";
-    std::string behind_int_rel = "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),parallel(displacement(ground(x),figure(x)),orientation(speaker(x))))";
-        std::string behind_int_rel_not_front = "or(parallel(displacement(figure(x),ground(x)),orientation(ground(x))),and(parallel(displacement(ground(x),figure(x)),orientation(speaker(x))),not(parallel(orientation(ground(x)),orientation(speaker(x))))))"; // No relative behind when intrinsic front
+    std::string behind_int = "parallel(displacement(figure(x),ground(x)),forward(ground(x)))";
+    std::string behind_int_rel = "or(parallel(displacement(figure(x),ground(x)),forward(ground(x))),parallel(displacement(ground(x),figure(x)),forward(speaker(x))))";
+        std::string behind_int_rel_not_front = "or(parallel(displacement(figure(x),ground(x)),forward(ground(x))),and(parallel(displacement(ground(x),figure(x)),forward(speaker(x))),not(parallel(forward(ground(x)),forward(speaker(x))))))"; // No relative behind when intrinsic front
 
     // Side
-    std::string side_int = "orthogonal(displacement(ground(x),figure(x)),orientation(ground(x)))";
-    std::string side_int_rel = "or(orthogonal(displacement(ground(x),figure(x)),orientation(ground(x))),orthogonal(displacement(ground(x),figure(x)),orientation(speaker(x))))";
+    std::string side_int = "orthogonal(displacement(ground(x),figure(x)),forward(ground(x)))";
+    std::string side_int_rel = "or(orthogonal(displacement(ground(x),figure(x)),forward(ground(x))),orthogonal(displacement(ground(x),figure(x)),forward(speaker(x))))";
 }
--- Models/Frames-of-Reference/DSL.h
+++ Models/Frames-of-Reference/DSL.h
@@ -2,10 +2,10 @@
 
 namespace DSL {
 
+    // Vector calculations
     const auto nonzero = +[](Vector x, Vector y) -> bool {
         return magnitude(x) == 0 || magnitude(y) == 0;
     };
-
     const auto displacement = +[](Object x, Object y) -> Vector {
         return y.location - x.location;
     };
@@ -21,4 +21,9 @@ namespace DSL {
         if (nonzero(x,y)) {return false;}
         return cosine_similarity(x,y) == 0;
     };
+
+    // Feature extraction
+    const auto forward = +[](Object x) -> Vector {return x.forward;};
+    const auto upward = +[](Object x) -> Vector {return x.upward;};
+    const auto rightward = +[](Object x) -> Vector {return x.rightward;};
 }
