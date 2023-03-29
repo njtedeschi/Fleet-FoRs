1ecdf2f0a09898e5e91af2574efa725ba66cb528
Nico Tedeschi <nicotedeschi@gmail.com>
2023-03-29 11:45:08 -0700

    added ARGUMENT_WEIGHT; set all weights to 1 by default

--- Models/Frames-of-Reference/MyGrammar.h
+++ Models/Frames-of-Reference/MyGrammar.h
@@ -5,8 +5,9 @@
 
 #include "DSL.h"
 
-double TERMINAL_WEIGHT = 3.0;
+double TERMINAL_WEIGHT = 1.0;
 double TERMINATING_WEIGHT = 1.0;
+double ARGUMENT_WEIGHT = 1.0;
 double UPWARD_WEIGHT = 1.0;
 double RIGHTWARD_WEIGHT = 1.0;
 double NEGATION_WEIGHT = 1.0;
@@ -22,13 +23,13 @@ public:
 		add("rightward(%s)", DSL::rightward, RIGHTWARD_WEIGHT);
                 // Doubled letter to distinguish from rules used in displacement expansions
 		add("SS(%s)",       +[](Scene x) -> OrientedObject { return x.speaker;});
-		add("GG(%s)",        +[](MyInput x) -> OrientedObject { return x.scene.ground;});
+		add("GG(%s)",        +[](MyInput x) -> OrientedObject { return x.scene.ground;}, TERMINAL_WEIGHT*ARGUMENT_WEIGHT);
 
                 add("parallel(%s,%s)", DSL::parallel, TERMINATING_WEIGHT);
-                add("+-parallel(%s,%s)", +[](Displacement x, Direction y) -> bool {
-                    if (DSL::nonzero(x,y)) {return false;}
-                    return cosine_similarity(x,y) == 1 || cosine_similarity(x,y) == -1;
-                        }, TERMINATING_WEIGHT);
+                /* add("+-parallel(%s,%s)", +[](Displacement x, Direction y) -> bool { */
+                /*     if (DSL::nonzero(x,y)) {return false;} */
+                /*     return cosine_similarity(x,y) == 1 || cosine_similarity(x,y) == -1; */
+                /*         }, TERMINATING_WEIGHT); */
                 /* add("antiparallel(%s,%s)", DSL::antiparallel); */
                 /* add("orthogonal(%s,%s)", DSL::orthogonal); */
 
@@ -46,14 +47,14 @@ public:
 		add("S(%s)",       +[](Scene x) -> Object { return x.speaker; });
 
 
-		add("F(%s)",        +[](MyInput x) -> Object { return x.scene.figure; }, TERMINAL_WEIGHT);
-		add("G(%s)",        +[](MyInput x) -> Object { return x.scene.ground; }, TERMINAL_WEIGHT);
+		add("F(%s)",        +[](MyInput x) -> Object { return x.scene.figure; }, TERMINAL_WEIGHT*ARGUMENT_WEIGHT);
+		add("G(%s)",        +[](MyInput x) -> Object { return x.scene.ground; }, TERMINAL_WEIGHT*ARGUMENT_WEIGHT);
                 add("G'(%s)", +[](MyInput x) -> Object {
                         if(x.scene.ground.is_participant) {
                             return Space::invalid_object;
                         }
                         return x.scene.ground;
-                        }, TERMINAL_WEIGHT);
+                        }, TERMINAL_WEIGHT*ARGUMENT_WEIGHT);
 		add("x",             Builtins::X<MyGrammar>);
 	}
 
