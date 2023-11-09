642068e1839174acb542bba5b5df950575339b6c
Nico Tedeschi <nicotedeschi@gmail.com>
2023-11-09 13:02:00 -0800

    updated script to run experiments with different odds values for intrinsic descriptions

--- Models/Frames-of-Reference/run_experiment.sh
+++ Models/Frames-of-Reference/run_experiment.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# Direct context probabilities
+p_direct_values=(0 0.2)
+
+# Intrinsic description probabilities
+# odds_values=(1/16 1/8 1/4 1/2 1 2 4 8 16)
+# Pitts
+# odds_values=(1/16 1/8 1/4)
+# Montague
+# odds_values=(1/2 1 2)
+# Wittgenstein
+# odds_values=(4 8 16)
+
+# Check if odds_values is defined
+if [ ${#odds_values[@]} -eq 0 ]; then
+  echo "Error: odds_values is not defined. Uncomment the appropriate odds_values line."
+  exit 1
+fi
+
+# Calculate probabilities from full odds
+for odds in "${odds_values[@]}"; do
+    # Check if odds is a fraction or a whole number
+    if [[ "$odds" == */* ]]; then
+        # Odds is a fraction, so calculate the probability accordingly
+        numerator=$(echo $odds | cut -d'/' -f1)
+        denominator=$(echo $odds | cut -d'/' -f2)
+        p_intrinsic_values+=( $(bc <<< "scale=9; $numerator / ($numerator + $denominator)") )
+    else
+        # Odds is a whole number, so calculate the probability accordingly
+        p_intrinsic_values+=( $(bc <<< "scale=9; $odds / (1 + $odds)") )
+    fi
+done
+
 # Get current datetime in the required format
 datetime=$(date +"%Y-%m-%d_%H-%M-%S")
 
@@ -7,47 +39,42 @@ datetime=$(date +"%Y-%m-%d_%H-%M-%S")
 root_dir="results/$datetime"
 mkdir -p "$root_dir"
 
-# Declare arrays for train_size and repetitions pairs
-train_sizes=(8 16 32 64 128 256 512)
-repetitions=(5 5 5 5 5 5 5)
-
-# Declare arrays for p_direct and p_intrinsic values
-p_direct_values=(0 0.2)
-p_intrinsic_values=(0.1 0.3 0.5 0.7 0.9)
-
 # Loop over p_direct values
 for p_direct in "${p_direct_values[@]}"; do
+    # Calculate percentage and format it to two digits
+    p_direct_percent=$(printf "%02d" $(echo "$p_direct * 100 / 1" | bc))
 
     # Loop over p_intrinsic values
-    for p_intrinsic in "${p_intrinsic_values[@]}"; do
+    for idx in "${!p_intrinsic_values[@]}"; do
+        p_intrinsic=${p_intrinsic_values[idx]}
+        odds=${odds_values[idx]}
+        
+        # Determine file name suffix based on whether odds is greater or less than 1
+        if [[ "$odds" == */* ]]; then
+            # Odds is a fraction, so use the format "1_x"
+            log_file_suffix=$(echo $odds | tr '/' '_')
+        else
+            # Odds is a whole number, so use the format "x_1"
+            log_file_suffix="${odds}_1"
+        fi
 
-        # Create a sub-directory for this combination of hyperparameters based on datetime
-        hyper_dir="$root_dir/$(date +"%Y-%m-%d_%H-%M-%S")"
-        mkdir -p "$hyper_dir"
+        log_file="$root_dir/dir_${p_direct_percent}-int_${log_file_suffix}.log"
 
-        # Loop over train_size and repetitions pairs
-        for i in "${!train_sizes[@]}"; do
-            train_size="${train_sizes[$i]}"
-            repetition="${repetitions[$i]}"
+        # Run the main executable with the specified arguments
+        ./main --chains=20 \
+               --threads=20 \
+               --steps=1000000 \
+               --p_frame=0.9 \
+               --p_intrinsic="$p_intrinsic" \
+               --p_direct="$p_direct" \
+               --train_min=10 \
+               --train_max=1000 \
+               --train_spacing=10 \
+               --top=1000 \
+               --language="english" \
+               --output_dir="$root_dir" 2>&1 | tee "$log_file"
 
-            # Create a log file for this combination of values
-            log_file="$hyper_dir/train_$train_size-rep_$repetition.log"
-
-            # Run the main executable with the specified arguments
-            ./main --chains=20 \
-                   --threads=20 \
-                   --steps=1000000 \
-                   --p_frame=0.9 \
-                   --p_intrinsic="$p_intrinsic" \
-                   --p_direct="$p_direct" \
-                   --train_size="$train_size" \
-                   --repetitions="$repetition" \
-                   --top=1000 \
-                   --language="english" \
-                   --output_dir="$hyper_dir" 2>&1 | tee "$log_file"
-
-            # Sleep for 1 second to ensure the next directory (if any) has a different datetime
-            sleep 1
-        done
+        # Sleep for 1 second to avoid potential issues with identical timestamps
+        sleep 1
     done
 done
