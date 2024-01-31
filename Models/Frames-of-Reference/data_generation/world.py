679d599b2de8fa851eaf15ea5da6aff73dd43350
Nico Tedeschi <nicotedeschi@gmail.com>
2024-01-31 09:35:10 -0800

    moved origin and cardinal direction vectors to separate file from other constants

---
+++ Models/Frames-of-Reference/data_generation/world.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+ORIGIN = np.array([0,0,0])
+EAST = np.array([1,0,0])
+WEST = np.array([-1,0,0])
+NORTH = np.array([0,1,0])
+SOUTH = np.array([0,-1,0])
+UP = np.array([0,0,1])
+DOWN = np.array([0,0,-1])
