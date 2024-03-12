c0017667b67764eab7738faa4b6ecb02b538ea3e
Nico Tedeschi <nicotedeschi@gmail.com>
2024-03-12 12:46:19 -0700

    updated data aggregation to use config file and to handle all metrics and simplified shares separately

--- Models/Frames-of-Reference/aggregate_data.py
+++ Models/Frames-of-Reference/aggregate_data.py
@@ -4,70 +4,74 @@ import argparse
 import os
 
 import pandas as pd
-# import yaml
+import yaml
 
-# TODO: get from config_aggregation.yaml file
-INPUT_SUBDIRECTORY = "test_results"
+# INPUT_SUBDIRECTORY = "test_results"
+INPUT_SUBDIRECTORY = "test_results_sorted"
 OUTPUT_SUBDIRECTORY = "aggregate_results"
 OUTPUT_FILENAME_METRICS = "all_metrics.csv"
 # Only include shares, and consolidate "SInt" and "MInt"
 OUTPUT_FILENAME_SHARES = "simplified_shares.csv"
 
 WORD_GROUPINGS = {
+    "above": "above/below",
+    "below": "above/below",
+    "front": "front/behind",
+    "behind": "front/behind",
+    "left": "left/right",
+    "right": "left/right",
+    "near": "near/far",
+    "far": "near/far"
+}
+WORD_GROUPINGS_SIMPLE = {
     # English
     "above": "above/below",
     "below": "above/below",
     "front": "front/behind",
     "behind": "front/behind",
-    "side": "side",
     "left": "left/right",
     "right": "left/right",
     "near": "near/far",
     "far": "near/far",
     # Mixtec
-    "head": "head",
-    "belly": "belly",
-    "face": "face",
-    "back": "back",
-    "flank": "flank"
+    "head": "above/below",
+    "belly": "above/below",
+    "face": "front/behind",
+    "back": "front/behind",
+    "flank": "side"
+}
+SENSE_GROUPINGS_SIMPLE = {
+    "SInt": "Int",
+    "MInt": "Int",
+    "SIntRel": "IntRel",
+    "MIntRel": "IntRel",
+    "SIntAbs": "IntAbs",
+    "MIntAbs": "IntAbs",
+    "SIntRelAbs": "IntRelAbs",
+    "MIntRelAbs": "IntRelAbs"
 }
 
-EXPERIMENTAL_CONDITIONS = {
-    "int_33_lang_0" : {
-        "Language": "English",
-        "%IntrinsicData": 33
-    },
-    "int_33_lang_1" : {
-        "Language": "Mixtec",
-        "%IntrinsicData": 33
-    },
-    "int_50_lang_0" : {
-        "Language": "English",
-        "%IntrinsicData": 50
-    },
-    "int_50_lang_1" : {
-        "Language": "Mixtec",
-        "%IntrinsicData": 50
-    },
-    "int_67_lang_0" : {
-        "Language": "English",
-        "%IntrinsicData": 67
-    },
-    "int_67_lang_1" : {
-        "Language": "Mixtec",
-        "%IntrinsicData": 67
-    }
-}
+def load_config(root):
+    try:
+        # Construct the path to the config.yaml file
+        config_path = os.path.join(
+            root,
+            'config_analysis.yaml'
+        )
+        with open(config_path, 'r') as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError:
+        print(f"Error: The file {config_path} was not found.")
+        return None
+    except yaml.YAMLError as exc:
+        print(f"Error parsing the YAML file: {exc}")
+        return None
 
-SENSE_GROUPINGS = {
-    ('SInt', 'MInt'): 'Int',
-    ('SIntRel', 'MIntRel'): 'IntRel'
-}
-
-def full_results_to_metrics(input_directory,
+def process_results(input_directory,
                             word_groupings,
+                            sense_groupings,
                             experimental_conditions):
-    rank_collapser = RankCollapser(word_groupings)
+    rank_collapser = RankCollapser(word_groupings, sense_groupings)
     concatenated_df = pd.DataFrame()
     for condition_label, subconditions in experimental_conditions.items():
         counts_path = os.path.join(input_directory,
@@ -83,50 +87,33 @@ def full_results_to_metrics(input_directory,
             collapsed_df.insert(insert_position, subcondition_label, subcondition_value)
             # Increment the position for the next column to be inserted
             insert_position += 1
-
-           # collapsed_df[subcondition_label] = subcondition_value
-
         concatenated_df = pd.concat([concatenated_df, collapsed_df], ignore_index=True)
     return concatenated_df
-
-def metrics_to_simplified_shares(df, sense_groupings):
-    # Filter the necessary columns
-    df_filtered = df[['Language', '%IntrinsicData', 'TrainingSize', 'Iteration', 'Word', 'Sense', 'Share']]
-
-    # Placeholder for the transformed rows
-    transformed_rows = []
-
-    # Iterate through the data and apply transformations
-    for _, group in df_filtered.groupby(['Language', '%IntrinsicData', 'TrainingSize', 'Iteration', 'Word']):
-        for original_senses, new_sense in sense_groupings.items():
-            # Filter rows that match the original senses
-            sense_rows = group[group['Sense'].isin(original_senses)]
-            if not sense_rows.empty:
-                # Sum the Share values and create a new row
-                new_share = sense_rows['Share'].sum()
-                new_row = sense_rows.iloc[0].copy()
-                new_row['Sense'] = new_sense
-                new_row['Share'] = new_share
-                transformed_rows.append(new_row)
-        # Remove the original SInt, MInt, SIntRel, MIntRel rows
-        group = group[~group['Sense'].isin(['SInt', 'MInt', 'SIntRel', 'MIntRel'])]
-        # Append remaining (untransformed) rows
-        transformed_rows.extend(group.to_dict('records'))
-
-    # Create a new DataFrame from the transformed rows
-    transformed_df = pd.DataFrame(transformed_rows)
-
-    return transformed_df
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('root', help="Path to the root directory") # e.g. results/DATE/
+    parser.add_argument('--simple',
+                        action='store_true',
+                        help=""
+    )
     # parser.add_argument('config', help="Name of config file in root")
     args = parser.parse_args()
 
     root = args.root
-    # config = args.config
+    config = load_config(root)
 
+    if args.simple:
+        word_groupings = WORD_GROUPINGS_SIMPLE
+        sense_groupings = SENSE_GROUPINGS_SIMPLE
+        output_filename = OUTPUT_FILENAME_SHARES
+    else:
+        word_groupings = WORD_GROUPINGS
+        sense_groupings = {}
+        output_filename = OUTPUT_FILENAME_METRICS
+    conditions_by_filename = config["conditions_by_filename"]
+
+    ###
     # TODO: Set constant values from config
     # Set input directory path
     input_directory = os.path.join(
@@ -143,25 +130,14 @@ if __name__ == "__main__":
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    metrics_filename = OUTPUT_FILENAME_METRICS
-    metrics_filepath = os.path.join(output_directory, metrics_filename)
-    shares_filename = OUTPUT_FILENAME_SHARES
-    shares_filepath = os.path.join(output_directory, shares_filename)
-
-    word_groupings = WORD_GROUPINGS
-    experimental_conditions = EXPERIMENTAL_CONDITIONS
-    sense_groupings = SENSE_GROUPINGS
+    output_filepath = os.path.join(output_directory, output_filename)
 
     ###
 
-    metrics_df = full_results_to_metrics(
+    results_df = process_results(
         input_directory,
         word_groupings,
-        experimental_conditions
+        sense_groupings,
+        conditions_by_filename
     )
-    shares_df = metrics_to_simplified_shares(
-        metrics_df,
-        sense_groupings
-    )
-    metrics_df.to_csv(metrics_filepath, index=False)
-    shares_df.to_csv(shares_filepath, index=False)
+    results_df.to_csv(output_filepath, index=False)
--- Models/Frames-of-Reference/dataprocessing/src/aggregation/rank_collapser.py
+++ Models/Frames-of-Reference/dataprocessing/src/aggregation/rank_collapser.py
@@ -4,10 +4,10 @@ from tqdm import tqdm
 
 class RankCollapser:
 
-    def __init__(self, word_groupings, chunksize=10000):
+    def __init__(self, word_groupings, sense_groupings):
         self.word_groupings = word_groupings
-        self.chunksize = chunksize
-
+        self.sense_groupings = sense_groupings
+        self.chunksize = 10000
 
     def process_experimental_condition(self, counts_path, posterior_lookup_path):
         counts_df = self.prepare_counts_df(counts_path)
@@ -17,7 +17,7 @@ class RankCollapser:
         posterior_groups = posterior_lookup_df.groupby(['TrainingSize', 'Iteration'])
 
         models = []
-        for ((counts_ts, counts_it), counts_group), ((posterior_ts, posterior_it), posterior_group) in zip(counts_groups, posterior_groups):
+        for ((counts_ts, counts_it), counts_group), ((posterior_ts, posterior_it), posterior_group) in tqdm(zip(counts_groups, posterior_groups), total=counts_groups.ngroups, desc="Processing Groups"):
             # Ensure matching groups are processed together
             assert counts_ts == posterior_ts and counts_it == posterior_it, "Mismatched groups"
             model_df = pd.merge(counts_group, posterior_group[['Rank', 'Posterior']], on='Rank', how='left')
@@ -25,28 +25,33 @@ class RankCollapser:
             model_df = self.collapse_model_ranks(model_df)
             models.append(model_df)
         collapsed_df = pd.concat(models, ignore_index=True)
-        # collapsed_df = self.pivot_and_flatten(collapsed_df)
-        # collapsed_df.reset_index()
         return collapsed_df
 
     #####
 
     def prepare_counts_df(self, counts_path):
-        words_to_include = self.word_groupings.keys()
-        filtered_rows = []
+        chunks = []
         for chunk in tqdm(pd.read_csv(counts_path, chunksize=self.chunksize),
                           desc="Input chunks read"):
-            filtered_chunk = chunk[chunk['Word'].isin(words_to_include)]
-            filtered_rows.append(filtered_chunk)
-        counts_df = pd.concat(filtered_rows, ignore_index=True)
-
+            chunks.append(chunk)
+        df = pd.concat(chunks, ignore_index=True)
         # Calculate whether accuracy is perfect for sense
-        counts_df['PerfectAccuracy'] = ((counts_df['FP'] == 0) & (counts_df['FN'] == 0)).astype(int)
+        df['PerfectAccuracy'] = ((df['FP'] == 0) & (df['FN'] == 0)).astype(int)
+        # Combine senses by group and set "PerfectAccuracy" by disjunction
+        if self.sense_groupings:
+            df['Sense'] = df['Sense'].replace(self.sense_groupings)
+            df = df.groupby(
+                    ['TrainingSize', 'Iteration', 'Rank', 'Word', 'Sense']
+                ).agg(
+                    {'PerfectAccuracy': 'max'}
+                ).reset_index()
         # Combine words by group and average their metrics
-        counts_df['Word'] = counts_df['Word'].apply(
-            lambda word: self.word_groupings.get(word, word))
-        counts_df = counts_df.groupby(['TrainingSize', 'Iteration', 'Rank', 'Word', 'Sense'], as_index=False).mean()
-        return counts_df
+        df['Word'] = df['Word'].replace(self.word_groupings)
+        df = df.groupby(
+                ['TrainingSize', 'Iteration', 'Rank', 'Word', 'Sense'],
+                as_index=False
+            ).mean()
+        return df
 
     # Takes group with fixed "TrainingSize" and "Iteration", and "Posterior" values merged in
     def collapse_model_ranks(self, model_df):
@@ -59,38 +64,57 @@ class RankCollapser:
         model_df['UnnormalizedProb'] = np.exp(model_df['StableLogProb'])
 
         # Normalize the probabilities
-        model_df['NormalizedProb'] = model_df.groupby(['Word', 'Sense'])['UnnormalizedProb'].transform(lambda x: x / x.sum())
+        model_df['NormalizedProb'] = model_df.groupby(
+            ['Word', 'Sense']
+        )['UnnormalizedProb'].transform(
+            lambda x: x / x.sum()
+        )
 
-        # Sum out ranks to get expected values
-        model_df = model_df.groupby(['TrainingSize', 'Iteration', 'Word', 'Sense']).apply(lambda group: pd.Series({
-            'E[TP]': np.sum(group['TP'] * group['NormalizedProb']),
-            'E[TN]': np.sum(group['TN'] * group['NormalizedProb']),
-            'E[FP]': np.sum(group['FP'] * group['NormalizedProb']),
-            'E[FN]': np.sum(group['FN'] * group['NormalizedProb']),
-            'Share': np.sum(group['PerfectAccuracy'] * group['NormalizedProb']),
-        })).reset_index()
+        # Sum out ranks to get expected values using .agg() instead of .apply()
+        metric_dict = {
+            'Share': 'PerfectAccuracy'
+        }
+        # Only calculate more advanced metrics if no sense grouping simplifications were made
+        calculate_advanced_metrics = not self.sense_groupings
+        if calculate_advanced_metrics:
+            counts = ['TP', 'TN', 'FP', 'FN']
+            for count in counts:
+                metric_dict[f'E[{count}]'] = count
+        # Precompute products for each metric with NormalizedProb
+        for original_metric in metric_dict.values():
+            product_column = f"{original_metric}_Prod"
+            model_df[product_column] = model_df[original_metric] * model_df['NormalizedProb']
 
-        model_df = self.calculate_metrics(model_df)
+        # Setup aggregation dictionary using precomputed product columns
+        agg_dict = {metric: (f"{original_metric}_Prod", 'sum') for metric, original_metric in metric_dict.items()}
+
+        model_df = model_df.groupby(
+            ['TrainingSize', 'Iteration', 'Word', 'Sense']
+        ).agg(**agg_dict).reset_index()
+        model_df = self.finalize_collapsed_df(model_df, calculate_advanced_metrics)
 
         return model_df
 
-    def calculate_metrics(self, df):
-        tp = df['E[TP]']
-        tn = df['E[TN]']
-        fp = df['E[FP]']
-        fn = df['E[FN]']
+    def finalize_collapsed_df(self, df, calculate_advanced_metrics=False):
+        if calculate_advanced_metrics:
+            tp = df['E[TP]'].to_numpy()
+            tn = df['E[TN]'].to_numpy()
+            fp = df['E[FP]'].to_numpy()
+            fn = df['E[FN]'].to_numpy()
 
-        precision = tp / (tp + fp).replace(0, np.nan)
-        recall = tp / (tp + fn).replace(0, np.nan)
-        accuracy = (tp + tn) / (tp + tn + fp + fn).replace(0, np.nan)
-        f1 = 2 * (precision * recall) / (precision + recall).replace(0, np.nan)
+            # Use np.divide for safe division, filling with NaN where division by zero would occur
+            precision = np.divide(tp, tp + fp, where=((tp + fp) != 0))
+            recall = np.divide(tp, tp + fn, where=((tp + fn) != 0))
+            accuracy = np.divide(tp + tn, tp + tn + fp + fn, where=((tp + tn + fp + fn) != 0))
+            f1_numerator = 2 * (precision * recall)
+            f1_denominator = precision + recall
+            f1 = np.divide(f1_numerator, f1_denominator, where=(f1_denominator != 0))
 
-        df['Precision'] = precision
-        df['Recall'] = recall
-        df['F1'] = f1
-        df['Accuracy'] = accuracy
+            df['Precision'] = precision
+            df['Recall'] = recall
+            df['F1'] = f1
+            df['Accuracy'] = accuracy
 
         # Drop the TP, TN, FP, FN columns
-        for metric in ['E[TP]', 'E[TN]', 'E[FP]', 'E[FN]']:
-            df.drop((metric), axis=1, inplace=True, errors='ignore')
+        df.drop(columns=['E[TP]', 'E[TN]', 'E[FP]', 'E[FN]'], inplace=True, errors='ignore')
         return df
